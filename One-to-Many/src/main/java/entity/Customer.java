5d8cb2a592bf3ccd3dec9b44ceeb1cd67a9610d7
Lahiru_Mudith <lahimudith@gmail.com>
2025-02-26 14:28:32 +0530

    load and get method different

--- One-to-Many/src/main/java/entity/Customer.java
+++ One-to-Many/src/main/java/entity/Customer.java
@@ -20,18 +20,22 @@ public class Customer {
     private String name;
 
     @OneToMany(mappedBy = "customer",cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    //             **Data Fetching**
-    //EAGER - load all the data from the database
-    //LAZY - load data only when it is needed
+    /*             **Data Fetching**
+    EAGER - load all the data from the database
+    LAZY - load data only when it is needed
+    */
 
-    //Customer customer1 = sesion.load(Customer.class,1) - LAZY loading
-    //customer.getName() - LAZY loading - run get name quary
-    //customer.getId() - LAZY loading - after get name...run get id quary
+    /*
+    Customer customer1 = sesion.load(Customer.class,1) - LAZY loading
+    customer.getName() - LAZY loading - run get name quary
+    customer.getId() - LAZY loading - after get name...run get id quary
+    */
 
-    //Customer customer2 = sesion.get(Customer.class,1) - EAGER loading
-    //customer.getName()
-    //customer.getId() - no need to run another quary
-
+    /*
+    Customer customer2 = sesion.get(Customer.class,1) - EAGER loading
+    customer.getName()
+    customer.getId() - no need to run another quary
+    */
 
     private List<Order> orderList;//because customer is many
 }
